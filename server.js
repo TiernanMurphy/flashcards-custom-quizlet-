1ef422c85cbb6972ebc41159cdf28def9625c924
books4free <156983724+books4free@users.noreply.github.com>
2026-02-14 00:24:56 -0800

    passport config setup

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+// node lets you run JS outside a browser, Express is a web framework built on top of Node
+// Express makes building web apps and APIs much easier (mainly HTTP, routing, and middleware stuff)
+
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config(); // load environment variables
+
 const app = express();  // app is an instance of express
+
+// mongoose connection, uses connection string as parameter, returns message based on results
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log("✅ Connected to MongoDB!"))
+    .catch((err) => console.error('❌ MongoDB connection error:', err));
 
 app.set('view engine', 'ejs');  // use EJS for templating
 app.use(express.static('public'));  // serve static files from /public
@@ -7,6 +20,11 @@ app.use(express.static('public'));  // serve static files from /public
 // route for homepage HTTP GET requests
 app.get('/', (req, res) => {
     res.render('index');  // respond to homepage GET request with index.ejs response
+});
+
+// test route
+app.get('/auth/google', (req, res) => {
+   res.render('test');
 });
 
 // starts server on port 3000
